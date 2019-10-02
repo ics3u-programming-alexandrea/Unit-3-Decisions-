7630bd1cc1d89a837e0bfdf6d382283bc674c008
s465640 <54953677+s465640@users.noreply.github.com>
2019-10-02 11:34:34 -0400

    Initial commit

---
+++ GuessNumberAlex/GuessNumberAlex/GuessNumberForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GuessNumberAlex
+{
+    partial class frmGuessNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.picAnswer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess an Interger between 1 and 10:";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(517, 95);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(108, 20);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(156, 183);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(103, 35);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(522, 201);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(47, 31);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "???";
+            this.lblAnswer.Click += new System.EventHandler(this.LblAnswer_Click);
+            // 
+            // picAnswer
+            // 
+            this.picAnswer.Location = new System.Drawing.Point(215, 253);
+            this.picAnswer.Name = "picAnswer";
+            this.picAnswer.Size = new System.Drawing.Size(312, 166);
+            this.picAnswer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAnswer.TabIndex = 4;
+            this.picAnswer.TabStop = false;
+            // 
+            // frmGuessNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGuessNumber";
+            this.Text = "Number Guessing Game";
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.PictureBox picAnswer;
+    }
+}
+
---
+++ GuessNumberAlex/GuessNumberAlex/GuessNumberForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberAlex
+{
+    public partial class frmGuessNumber : Form
+    {
+        public frmGuessNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            this.lblAnswer.Hide();
+            this.picAnswer.Hide();
+
+           //declare constants and variables
+            const int Correct_Number = 6;
+            int numRandom;
+
+            //get the guess number from the textbox
+            numRandom = int.Parse(txtGuess.Text);
+
+            //if the number guessed is 6, display you guessed correctly
+            this.lblAnswer.Show();
+            this.picAnswer.Show();
+
+            if (numRandom == Correct_Number)
+            {
+                this.lblAnswer.Text = "You guessed correctly";
+                this.picAnswer.Image = Properties.Resources.check;
+            }
+            else
+            {
+                this.lblAnswer.Show();
+                this.picAnswer.Show();
+                this.lblAnswer.Text = "You are Incorrect ";
+                this.picAnswer.Image = Properties.Resources.red;
+            }
+
+        }
+
+        private void LblAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
