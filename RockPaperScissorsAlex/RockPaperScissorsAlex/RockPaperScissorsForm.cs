738eb56096b0530b79916851631a433351ba91c4
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-10-06 19:26:33 -0400

    added rock paper scissors

--- RockPaperScissorsAlex/RockPaperScissorsAlex/RockPaperScissorsForm.cs
+++ RockPaperScissorsAlex/RockPaperScissorsAlex/RockPaperScissorsForm.cs
@@ -12,62 +12,15 @@ namespace RockPaperScissorsAlex
 {
     public partial class frmRockPaperScissors : Form
     {
-        //declear global variables and constants
-        const int MIN_VALUE = 1;
-        const int MAX_VALUE = 3;
-        Random randomNumberGenerator;
-        public frmRockPaperScissors()
-        {
-            InitializeComponent();
-            //create the random number generator object
-            randomNumberGenerator = new Random();
-            this.lblComment.Hide();
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void frmRockPaperScissors_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-            
-        {
-            //declear local variables and constants
-            int playerChoice, computerChoice;
-            const int ROCK = 1;
-            const int PAPER= 2;
-            const int SCISSORS = 3;
-
-            //Get the user's selection
-            if (radRock.Checked == true);
-            if (radCRock.Checked == true) ;
-            this.lblComment.Text = "Its a Tie";
-           
-            
+        
 
 
 
 
 
-        }
 
-        private void grbCChoice_Enter(object sender, EventArgs e)
-        {
+            }
+
 
         }
     }
