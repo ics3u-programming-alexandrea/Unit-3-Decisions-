c82068ac8dd62f8f9bb67d2994439470411180e0
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-10-06 10:32:52 -0400

    added Guess Number

--- RockPaperScissorsAlex/RockPaperScissorsAlex/Program.cs
+++ RockPaperScissorsAlex/RockPaperScissorsAlex/Program.cs
@@ -1,23 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GuessNumberAlexandrea
+namespace RockPaperScissorsAlex
 {
-    static class GuessNumber
+    static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GuessNumberForm());
+            Application.Run(new Form1());
         }
     }
 }
