732295e95aab92f5fe2b1849b97f9fbfca14fe88
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-10-10 21:14:02 -0400

    added perfect Squares

---
+++ PerfectSquaresAlex/PerfectSquaresAlex/PerfectSquaresForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PerfectSquaresAlex
+{
+    partial class frmPerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudNumbers = new System.Windows.Forms.NumericUpDown();
+            this.lstSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumbers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(250, 47);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudNumbers
+            // 
+            this.nudNumbers.Location = new System.Drawing.Point(250, 125);
+            this.nudNumbers.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumbers.Name = "nudNumbers";
+            this.nudNumbers.Size = new System.Drawing.Size(120, 20);
+            this.nudNumbers.TabIndex = 1;
+            this.nudNumbers.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lstSquares
+            // 
+            this.lstSquares.FormattingEnabled = true;
+            this.lstSquares.Location = new System.Drawing.Point(206, 173);
+            this.lstSquares.Name = "lstSquares";
+            this.lstSquares.Size = new System.Drawing.Size(208, 147);
+            this.lstSquares.TabIndex = 2;
+            // 
+            // frmPerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.lstSquares);
+            this.Controls.Add(this.nudNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPerfectSquares";
+            this.Text = "Perfect Squares by Alex";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumbers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudNumbers;
+        private System.Windows.Forms.ListBox lstSquares;
+    }
+}
+
---
+++ PerfectSquaresAlex/PerfectSquaresAlex/PerfectSquaresForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by: Alex Ude
+ * Created on: 10-October-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect squares with while loop
+ * This program displays the perfect squares in between the user's value and the minumum value
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquaresAlex
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declear local variables
+            int endingValue;
+            int value =1;
+            Double squareRootasDouble;
+            int squareRootasInteger;
+            //clear all items from the listbox
+            lstSquares.Items.Clear();
+            endingValue = Convert.ToInt32(this.nudNumbers.Value);
+
+            //contiune stating all perfect squares from min Value to the user's value
+            while (value <= endingValue) 
+            {
+                //take squareroot of the value
+                squareRootasDouble = Math.Sqrt(value);
+
+                //convert double to an integer
+                squareRootasInteger = Convert.ToInt32(squareRootasDouble);
+                //the only way for both the decimal and the integer to be equal is if the value was a perfect square 
+                if (squareRootasInteger == squareRootasDouble)
+                {
+                    this.lstSquares.Items.Add(value + " is a perfect square");
+                    this.Refresh();
+                }
+                value = value + 1;
+            }
+        }  
+    }
+}
