6d9f2fd24710e507edc30225846bee0f2a7c1a82
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-10-10 20:12:52 -0400

    Added Factorial Do While

--- FactorialDoWhileAlex/FactorialDoWhileAlex/FactorialDoWhileForm.Designer.cs
+++ FactorialDoWhileAlex/FactorialDoWhileAlex/FactorialDoWhileForm.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.lblEnter = new System.Windows.Forms.Label();
-            this.txtEnter = new System.Windows.Forms.TextBox();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
             this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lstFactors = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // lblEnter
@@ -45,51 +45,58 @@
             this.lblEnter.Size = new System.Drawing.Size(150, 18);
             this.lblEnter.TabIndex = 0;
             this.lblEnter.Text = "Enter the Number: ";
+            this.lblEnter.UseWaitCursor = true;
             // 
-            // txtEnter
+            // txtNumber
             // 
-            this.txtEnter.Location = new System.Drawing.Point(203, 32);
-            this.txtEnter.Name = "txtEnter";
-            this.txtEnter.Size = new System.Drawing.Size(100, 20);
-            this.txtEnter.TabIndex = 1;
+            this.txtNumber.Location = new System.Drawing.Point(203, 32);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.UseWaitCursor = true;
             // 
-            // btnStart
+            // btnCalculate
             // 
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(86, 77);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(76, 29);
-            this.btnStart.TabIndex = 2;
-            this.btnStart.Text = "Start";
-            this.btnStart.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(89, 88);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(98, 29);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.UseWaitCursor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
-            // label1
+            // lblAnswer
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 123);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 18);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Multiply the following numbers:";
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(136, 265);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(26, 18);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "??";
+            this.lblAnswer.UseWaitCursor = true;
+            this.lblAnswer.Click += new System.EventHandler(this.label1_Click);
             // 
-            // progressBar1
+            // lstFactors
             // 
-            this.progressBar1.Location = new System.Drawing.Point(57, 152);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(203, 146);
-            this.progressBar1.TabIndex = 4;
+            this.lstFactors.FormattingEnabled = true;
+            this.lstFactors.Location = new System.Drawing.Point(71, 134);
+            this.lstFactors.Name = "lstFactors";
+            this.lstFactors.Size = new System.Drawing.Size(151, 108);
+            this.lstFactors.TabIndex = 4;
             // 
             // frmFactorial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(315, 310);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.txtEnter);
+            this.Controls.Add(this.lstFactors);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.lblEnter);
             this.Name = "frmFactorial";
             this.Text = "Factorial Do While By Alex";
@@ -102,11 +109,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblEnter;
-        private System.Windows.Forms.TextBox txtEnter;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
         private System.DirectoryServices.DirectoryEntry directoryEntry1;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox lstFactors;
     }
 }
 
--- FactorialDoWhileAlex/FactorialDoWhileAlex/FactorialDoWhileForm.cs
+++ FactorialDoWhileAlex/FactorialDoWhileAlex/FactorialDoWhileForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Alexandrea Udechukwu
+ * Created on: 10-October-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program calculates the factorial of a number entered by a user
+ * and diaplays the numbers multiplied
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,43 @@ namespace FactorialDoWhileAlex
         public frmFactorial()
         {
             InitializeComponent();
+            //hide your answer label
+            this.lblAnswer.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declear local variables
+            Double factorialAnswer = 1;
+            Double factorialNumber;
+            int factorialCounter = 0;
+
+            this.lstFactors.Items.Clear();
+            // get the user's number
+            factorialNumber = Convert.ToDouble(this.txtNumber.Text);
+            do
+            {
+                //increment the counter by 1
+                factorialCounter = factorialCounter + 1;
+
+                //multiply the counter by the next increament until number reaches the user's number
+                factorialAnswer = factorialAnswer * factorialCounter;
+
+                //list the number for user to see
+                this.lstFactors.Items.Add(factorialCounter);
+
+                //display the answers in a label
+                this.lblAnswer.Show();
+                lblAnswer.Text = Convert.ToString(factorialNumber) + "! = " + Convert.ToString(factorialAnswer);
+            } while (factorialCounter < factorialNumber) ;
+            
+            
+            
         }
     }
 }
