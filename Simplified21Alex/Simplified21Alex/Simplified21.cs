d7de1d4807df006dc18a17d96140fe05720a7dd8
Alexandrea <54953677+s465640@users.noreply.github.com>
2019-10-27 19:49:10 -0400

    Added simplified 21

--- Simplified21Alex/Simplified21Alex/Simplified21.cs
+++ Simplified21Alex/Simplified21Alex/Simplified21.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Simplified21Alex
 {
-    static class Program
+    static class Simplified21
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +17,7 @@ namespace Simplified21Alex
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmSimplified21());
         }
     }
 }
