eac252c72c0e7f43cabe32c0a72a87709083ba6b
s465640 <54953677+s465640@users.noreply.github.com>
2019-10-28 11:26:57 -0400

    Added Running Average

--- RunningAverageAlex/RunningAverageAlex/RunningAverageForm.cs
+++ RunningAverageAlex/RunningAverageAlex/RunningAverageForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Alex ude
+ * Created on: 25-October-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Running Average
+ * This program calculates the average of numbers inputed into the textbox
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,9 +23,6 @@ namespace RunningAverageAlex
         double Sum = 0;
         double amountScores = 0;
 
-
-
-
         public frmRunningAverage()
         {
             InitializeComponent();
@@ -28,22 +32,32 @@ namespace RunningAverageAlex
         }
        
         
-        
-        
-
-
         private void BtnCalculate_Click(object sender, EventArgs e)
-        {//set local var
+        {//set local variables
             double score;
             double average = 0;
-
+            //parse the textbox
             score = double.Parse(this.txtScore.Text);
+            //make a counter
             amountScores++;
-            
+            //set up the formular for the sum
             Sum= Sum + score;
+            // set up the formular for the average
             average =Sum / amountScores;
+
             lblAverage.Text = Convert.ToString(average);
+            //show the label that displays the average
             this.lblAverage.Show();
+
+            //if the user enters -1 display a goodbye message and disable buttons
+            if(score==-1)
+            {
+                MessageBox.Show("Running average ended.", "Running average");
+                this.btnCalculate.Enabled = false;
+                this.txtScore.Visible = false;
+            }
+            
+
             
 
 
